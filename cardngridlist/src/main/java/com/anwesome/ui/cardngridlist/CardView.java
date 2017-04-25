b03e26a83f935e41f34f2b6b855fb676f238001b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 11:41:35 +0530

    Implementing basic methods for CardView

--- cardngridlist/src/main/java/com/anwesome/ui/cardngridlist/CardView.java
+++ cardngridlist/src/main/java/com/anwesome/ui/cardngridlist/CardView.java
@@ -6,18 +6,39 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anweshmishra on 25/04/17.
  */
 public class CardView extends View {
-    public CardView(Context context) {
+    private List<Card> cards = new ArrayList<>();
+    private int time = 0;
+    public CardView(Context context,List<Card> cardList) {
         super(context);
+        this.cards = cardList;
     }
     public void onDraw(Canvas canvas) {
         Paint paint = Constants.paint;
-
+        if(time == 0) {
+            initLayout(canvas.getWidth());
+        }
+        for(Card card:cards) {
+            card.draw(canvas,paint);
+        }
+        time++;
+    }
+    public void initLayout(int w) {
+        
     }
     public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            handleTap(event.getX(),event.getY());
+        }
         return true;
+    }
+    protected void handleTap(float x,float y) {
+
     }
 }

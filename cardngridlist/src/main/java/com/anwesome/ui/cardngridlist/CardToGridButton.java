51ee0ed0e2c437ed357aae3d110cd12bde04b02e
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 10:24:44 +0530

    Adding the button

---
+++ cardngridlist/src/main/java/com/anwesome/ui/cardngridlist/CardToGridButton.java
@@ -0,0 +1,63 @@
+package com.anwesome.ui.cardngridlist;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 25/04/17.
+ */
+public class CardToGridButton extends View {
+    private int w;
+    private CardToGridShape cardToGridShape = new CardToGridShape();
+    public CardToGridButton(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        Paint paint = Constants.paint;
+        w = canvas.getWidth();
+        cardToGridShape.draw(canvas,paint);
+    }
+    public void update(float factor) {
+        cardToGridShape.update(factor);
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+    private class CardToGridShape {
+        private float l = 0,deg = 0;
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.drawColor(Color.parseColor("#BDBDBD"));
+            paint.setColor(Color.WHITE);
+            paint.setStrokeWidth(w/15);
+            canvas.save();
+            canvas.translate(w/2,w/2);
+            canvas.rotate(deg);
+            for(int i =0;i<3;i++) {
+                canvas.save();
+                canvas.translate(0,(i-1)*w/2);
+                Path path = new Path();
+                for(int k=0;k<3;k++) {
+                    int x = (i-1)*w/2, y = 0;
+                    canvas.save();
+                    canvas.translate(x,y);
+                    canvas.drawRect(new RectF(-w/30,-w/30,w/30,w/30),paint);
+                    canvas.restore();
+                }
+                canvas.drawLine(0,0,-l,0,paint);
+                canvas.drawLine(0,0,l,0,paint);
+                canvas.restore();
+            }
+            canvas.restore();
+        }
+        public void update(float factor) {
+            deg = 90*factor;
+            l = ((w/2-w/15))*factor;
+        }
+    }
+}
